a586df7b7b89e5d31d296af831b378ccd918d355
vker <vker123@163.com>
2025-01-20 22:54:50 +0800

    fix: 修复登录接口返回类型，使用 ApiResult 作为返回值

--- src/api/controllers/auth.rs
+++ src/api/controllers/auth.rs
@@ -1,10 +1,8 @@
-
 use actix_web::web;
 
 use crate::api::{dto::auth::UserDto, resp::ApiResult};
 
-
-pub fn login() -> ApiResult<UserDto> {
+pub async fn login() -> ApiResult<UserDto> {
     let d = UserDto{ uid: 101 };
     Ok(web::Json(d.into()))
 }
